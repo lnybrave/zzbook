9be53e78950ecb827728ed5e596a1b1fb6adba87
lnybrave <lnybrave@gmail.com>
2017-08-15 17:59:22 +0800

    settings

--- ebook/settings.py
+++ ebook/settings.py
@@ -156,6 +156,9 @@ DATE_FORMAT = 'Y-m-d'
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
