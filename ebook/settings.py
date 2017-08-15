d41404d7e8b1c2a49d15c814faf6447192b8f807
lnybrave <lnybrave@gmail.com>
2017-08-15 18:12:36 +0800

    settings

--- ebook/settings.py
+++ ebook/settings.py
@@ -159,6 +159,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
